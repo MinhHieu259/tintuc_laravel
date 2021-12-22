22268b32ce54601d7d17a5431f00ea3c2f731324
MinhHieu259 <mhieu7252@gmail.com>
2021-12-22 10:35:08 +0700

    hieuaddd

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//Người dùng
+Route::get('/','HomeController@index');
+Route::get('/trang-chu','HomeController@index');
