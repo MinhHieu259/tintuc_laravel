59001889f5ef74601d8a826a9054864b8662ffca
MinhHieu259 <mhieu7252@gmail.com>
2021-12-30 10:18:54 +0700

    ádns

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,12 +11,12 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $tintucFeature = News::where('id_category',1)->first();
-        $doanhoiFeature = News::where('id_category',2)->first();
-        $hoctapbacFeature = News::where('id_category',3)->first();
-        $tuyentruyenFeature = News::where('id_category',4)->first();
+        $tintucFeature = News::where('id_category',1)->orderBy('id_news','DESC')->first();
+        $doanhoiFeature = News::where('id_category',2)->orderBy('id_news','DESC')->first();
+        $hoctapbacFeature = News::where('id_category',3)->orderBy('id_news','DESC')->first();
+        $tuyentruyenFeature = News::where('id_category',4)->orderBy('id_news','DESC')->first();
         $Category = Category::all();
-        $newByCategory = News::all();
+        $newByCategory = News::orderBy('id_news','DESC')->get();
         $featuredNew = News::where('featured','=','1')->get();
         return view('pages.home',)->with(compact('tintucFeature','doanhoiFeature','hoctapbacFeature','tuyentruyenFeature','newByCategory','Category', 'featuredNew'));
     }
